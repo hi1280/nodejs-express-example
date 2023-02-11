92a657ecd387674afe8bdd54395954360d9127a9
hi1280 <6506310+hi1280@users.noreply.github.com>
2023-02-11 18:12:16 +0900

    fix

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', async function(req, res, next) {
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
+    database : process.env.DB_DATABASE ?? 'world'
   });
   const [countries] = await connection.execute('SELECT * from country');
   res.render('index', { title: 'Express', countries });
